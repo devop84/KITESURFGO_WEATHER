3fe7e7303e8a3f541a4e5ef96100df384a2a5085
VALENTIN Francois <valentinpro@gmail.com>
2023-04-20 11:55:39 -0300

    test

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ const selectLocation = document.querySelector('.select-location');
 toggleSelectLocation.addEventListener('click', () => {
   selectLocation.classList.toggle('display-select-location');
 });
+
+
+const myLocation = document.getElementById('my_location');
+
+myLocation.addEventListener('click', getLocation);
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(setPosition);
+  } else {
+    alert('Geolocation is not supported by this browser.');
+  }
+}
+
+function setPosition(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const inputField = document.querySelector('input[type="text"]');
+  inputField.value = `${latitude}, ${longitude}`;
+}
+
+
+
